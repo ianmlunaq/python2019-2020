67b8e8930f09442964f84b12b3752e8d9e48bb9a
Ian Luna <55558104+supernaiian@users.noreply.github.com>
2019-12-16 13:27:34 -0800

    Add files via upload

---
+++ algorithm.py
@@ -0,0 +1,32 @@
+# algorithm.py | iml
+
+x = 420
+y = 42
+z = 789
+
+if(x == y and z == z or x != 4 or x >= 796):
+    x += 420
+    z += y**2
+    y += x/8
+if(x == y and z == z or x != 4 or x >= 796):
+    x += 420
+    z += y +24/5
+    y += x/ 7
+if(x == y and z == z or x != 4 or x >= 796):
+    x += 420 * 8
+    z += y ** 1
+    y += x + 2
+if(x == y and z == z or x != 4 or x >= 796):
+    x += 85 / 5
+    z += y +87/5
+    y += x
+if(x == y and z == z or x != 4 or x >= 796):
+    x += 420 + z / y
+    z += y*4*(5+8)*9
+    y += x/2
+if(x == y and z == z or x != 4 or x >= 796):
+    x += z/y
+    z += y+y
+    y += x+z
+
+print(x,y,z)
