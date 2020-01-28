5e3168da6cae81e31f1faebfaae6300ea8b2e41a
Ian Luna <ianmlunaq@gmail.com>
2020-01-28 13:10:54 -0800

    Add files via upload

---
+++ namechangev2.py
@@ -0,0 +1,22 @@
+namestart= 1026250
+
+fh = open("/opt/minecraft-pi/minecraft-pi", "r+b")
+fh.seek(namestart)
+
+print ("Current name: {} ".format(fh.read(7)))
+
+piname = raw_input("New player name: ")
+
+if len(piname) > 1:
+    if len(piname) > 7: piname = piname[:7]
+    if len(piname) < 7:
+        extraspaces = 7 - len(piname)
+        for x in xrange(0,extraspaces):
+            piname = piname + " "
+    print ("Changed name to:" + piname + " - " + str(len(piname)))
+    fh.seek(namestart)
+    fh.write(piname)
+else:
+    print ("No new name given")
+
+fh.close()
